34bb4a667bb335e37e3db73d899fa3f45d85a7e4
John Burke <john.burke@erieinsurance.com>
2017-11-07 14:08:50 -0500

    added some ppt

--- TheWildeyesBand/ClientApp/components/ImageSection.tsx
+++ TheWildeyesBand/ClientApp/components/ImageSection.tsx
@@ -3,7 +3,7 @@ import * as image from '../images/wildeyes_title.jpg';
 export class ImageSection extends React.Component<{}, {}> {
     public render() {
         return <div className="section-header">
-            <img width="100px" height="100px" src="{require(url('{image}')}"/>
+            <img width="100px" height="100px" src={image}/>
                </div>;
     }
 }
